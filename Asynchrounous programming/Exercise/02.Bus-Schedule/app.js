3b00ea693c18f90aede73f4462c93e8e0dbea507
flnx <32278279+flnx@users.noreply.github.com>
2022-10-31 12:33:50 +0200

    submit solution

--- Asynchrounous programming/Exercise/02.Bus-Schedule/app.js
+++ Asynchrounous programming/Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,35 @@
 function solve() {
+  const next = document.getElementsByClassName('info')[0];
+  const departBtn = document.getElementById('depart');
+  const arriveBtn = document.getElementById('arrive');
+  let stop = {
+    next: 'depot',
+  };
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  function depart() {
+    departBtn.disabled = true;
+    arriveBtn.disabled = false;
+    const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        stop = Object.assign({}, data);
+        next.textContent = `Next stop ${stop.name}`;
+      })
+      .catch(err => console.log(err)) 
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+    next.textContent = `Arriving at ${stop.name}`;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
