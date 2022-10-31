8f4f650480e239ad076d525ac860ca71f4914942
flnx <32278279+flnx@users.noreply.github.com>
2022-10-31 10:53:09 +0200

    Adding exercise

--- Asynchrounous programming/Exercise/01.Bus-Stop/app.js
+++ Asynchrounous programming/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
 function getInfo() {
-    console.log("TODO...");
+  const stopId = document.getElementById('stopId');
+  const stopNameElem = document.getElementById('stopName');
+  const ulBuses = document.getElementById('buses');
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then(displayBusInfo)
+    .catch((err) => {
+      ulBuses.innerHTML = '';
+      stopNameElem.textContent = 'Error';
+    });
+
+  function displayBusInfo(data) {
+    const buses = data.buses;
+    stopNameElem.textContent = data.name;
+    ulBuses.innerHTML = '';
+
+    Object.keys(buses).forEach((bus) => {
+      const li = document.createElement('li');
+      li.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+      ulBuses.appendChild(li);
+    });
+  }
 }
