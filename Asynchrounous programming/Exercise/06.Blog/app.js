49238aa35ff5e82971d1b59fe981dc45ad42e26b
flnx <32278279+flnx@users.noreply.github.com>
2022-11-01 17:38:41 +0200

    Adding exercises

--- Asynchrounous programming/Exercise/06.Blog/app.js
+++ Asynchrounous programming/Exercise/06.Blog/app.js
@@ -1,6 +1,79 @@
-function attachEvents() {
-    console.log('TODO...');
-    
+const select = document.getElementById('posts');
+const title = document.getElementById('post-title');
+const postComments = document.getElementById('post-comments');
+document.getElementById('btnLoadPosts').addEventListener('click', getPosts);
+document.getElementById('btnViewPost').addEventListener('click', getComments);
+
+function loadPosts({ id, title }) {
+  const option = document.createElement('option');
+  option.value = id;
+  option.textContent = title;
+  select.appendChild(option);
 }
 
-attachEvents();
+function loadComments({ id, text }) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  li.id = id;
+  postComments.appendChild(li);
+}
+
+async function getPosts() {
+  try {
+    const res = await fetch('http://localhost:3030/jsonstore/blog/posts');
+
+    if (!res.ok) {
+      throw new Error('Error')
+    }
+
+    const data = await res.json();
+    select.innerHTML = '';
+    Object.values(data).forEach((obj) => loadPosts(obj));
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function getComments() {
+  try {
+    const res = await fetch('http://localhost:3030/jsonstore/blog/comments');
+
+    if (!res.ok) {
+      throw new Error('Error')
+    }
+
+    const data = await res.json();
+    
+    const comments = Object.values(data)
+      .filter((x) => x.postId == select.value);
+
+    const postTitle = Array.from(select.options)
+      .find((x) => x.value == select.value);
+
+    title.textContent = postTitle.textContent;
+    setBodyDetails();
+
+    postComments.innerHTML = '';
+    Object.values(comments).forEach((x) => loadComments(x));
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function setBodyDetails() {
+  try {
+    const res = await fetch(`http://localhost:3030/jsonstore/blog/posts`);
+
+    if (!res.ok) {
+      throw new Error('Error')
+    }
+  
+    const data = await res.json();
+    const bodyTitle = Object.values(data)
+      .find((obj) => obj.id == select.value);
+
+    document.getElementById('post-body').textContent = bodyTitle.body;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
